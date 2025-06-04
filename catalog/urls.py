e1eefc0ffabfea3a1dcd91069a390f4c1f0eb3d6
Dipak Prajapati <137250781+dipak1903@users.noreply.github.com>
2025-06-04 22:01:43 +0530

    Delete catalog directory

--- catalog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from catalog.views import *
-
-urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('catalog/', ProductsView.as_view(), name='catalog'),
-]
